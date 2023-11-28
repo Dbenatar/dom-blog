4cc591e3ef9afce75f477407786dbd9c35ea8802
Dom <dom.benatar@hotmail.com>
2023-11-28 15:14:45 +0000

    vercelling

--- src/app/api/create-comments-table/route.ts
+++ src/app/api/create-comments-table/route.ts
@@ -2,7 +2,7 @@ import { sql } from "@vercel/postgres";
 import { NextResponse } from "next/server";
 
 export async function GET(request: Request) {
-  await sql`CREATE TABLE comments ( 
+  await sql`CREATE TABLE IF NOT EXISTS comments ( 
     id varchar(255),
     slug varchar(255),
     username varchar(255),
--- src/components/Comments.tsx
+++ src/components/Comments.tsx
@@ -1,4 +1,20 @@
 export default async function Comments({ postSlug }: { postSlug: string }) {
+  const WEBSTIRE_URL = "http://localhost:3000";
+
+  let comments = [];
+
+  try {
+    const commentsResult = await fetch(
+      `${WEBSTIRE_URL}/api/comments/${postSlug}`,
+      { next: { revalidate: 5 } }
+    );
+    const response = await commentsResult.json();
+    console.log(response);
+    comments = response.comments.rows;
+  } catch (err) {
+    console.log(err);
+  }
+
   return (
     <div>
       <h2>Comments</h2>
@@ -6,7 +22,7 @@ export default async function Comments({ postSlug }: { postSlug: string }) {
 
       <form action={`/api/comments/${postSlug}`} method="POST">
         <label htmlFor="username">Name</label>
-        <input type="text" name="username" />
+        <input type="text" name="username" className="text-black" />
 
         <label htmlFor="comment">Your comment</label>
         <textarea
@@ -19,6 +35,15 @@ export default async function Comments({ postSlug }: { postSlug: string }) {
 
         <button type="submit">Send comment</button>
       </form>
+      {comments.map((comment) => {
+        return (
+          <li key={comment.id}>
+            {comment.username} says...
+            <br />
+            {comment.content}
+          </li>
+        );
+      })}
     </div>
   );
 }
