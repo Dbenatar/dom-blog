ec98c4ea212a426ccbc1aff84f16f1dcea0964cb
Dom <dom.benatar@hotmail.com>
2023-11-29 09:53:49 +0000

    typos

--- src/components/Comments.tsx
+++ src/components/Comments.tsx
@@ -1,15 +1,14 @@
 import { WEB_SITE } from "config";
 
 export default async function Comments({ postSlug }: { postSlug: string }) {
-  const WEBSITE_URL = "http://localhost:3000";
+  const WEB_SITE = "http://localhost:3000";
 
   let comments = [];
 
   try {
-    const commentsResult = await fetch(
-      `${WEBSITE_URL}/api/comments/${postSlug}`,
-      { next: { revalidate: 5 } }
-    );
+    const commentsResult = await fetch(`${WEB_SITE}/api/comments/${postSlug}`, {
+      next: { revalidate: 5 },
+    });
     const response = await commentsResult.json();
     console.log(response);
     comments = response.comments.rows;
