d45856c9045ec5937fbbcdbabf50c0255e69094d
Dom <dom.benatar@hotmail.com>
2023-11-29 11:09:06 +0000

    link homepage

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -16,6 +16,7 @@ export default function Page() {
           );
         })}
       </ul>
+      <Link href="/">Go Home</Link>
     </div>
   );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
-import Image from "next/image";
+import Link from "next/link";
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1>Dom&apos;s Blog</h1>
+      <Link href="/blog">
+        <h1>Dom&apos;s Blog</h1>
+      </Link>
     </main>
   );
 }
